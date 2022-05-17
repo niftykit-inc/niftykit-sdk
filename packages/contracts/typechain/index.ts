b06b5950343bf814de8a9401b0e174e15e05179b
jcurbelo <curbelorobin@gmail.com>
2022-05-17 15:13:52 -0400

    Added @niftykit/contracts package

---
+++ packages/contracts/typechain/index.ts
@@ -0,0 +1,2 @@
+export type { TokenCollection } from "./TokenCollection";
+export { TokenCollection__factory } from "./factories/TokenCollection__factory";
--- packages/redeemable/src/Redeemable.ts
+++ packages/redeemable/src/Redeemable.ts
@@ -1,2 +1,29 @@
+import { Provider } from '@ethersproject/providers';
+import { TokenCollection, TokenCollection__factory } from '@niftykit/contracts/typechain';
+import { Signer } from 'ethers';
+
 export default class Redeemable {
+  contract: TokenCollection = {} as TokenCollection;
+
+  private async init(
+    signerOrProvider: Signer | Provider,
+    contractAddress: string
+  ): Promise<void> {
+    this.contract = TokenCollection__factory.connect(
+      contractAddress,
+      signerOrProvider
+    )
+    if (!this.contract) {
+      throw new Error('Initialization failed.');
+    }
+  }
+
+  static async create(
+    signerOrProvider: Signer | Provider,
+    contractAddress: string
+  ): Promise<Redeemable | null> {
+    const instance = new Redeemable();
+    await instance.init(signerOrProvider, contractAddress);
+    return instance;
+  }
 }
