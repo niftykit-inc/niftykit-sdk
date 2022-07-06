2b2932659f7403cc9db421966c32473f371a6c1d
jcurbelo <curbelorobin@gmail.com>
2022-07-06 18:16:50 -0400

    adds proper contracts package setup

---
+++ packages/contracts/config/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: './typechain/index.ts',
+  output: {
+    path: path.resolve(__dirname, '../dist/umd'),
+    filename: 'index.js',
+    library: 'Typechain',
+    libraryTarget: 'umd',
+    libraryExport: "default",
+    globalObject: 'this',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x*)?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            configFile: 'config/tsconfig.umd.json',
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.tsx', '.jsx'],
+  },
+}
