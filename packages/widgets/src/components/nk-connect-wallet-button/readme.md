0c2ec073093558bfe3f3c54720135ead0ccbe3eb
Terence Pae <terencepae@gmail.com>
2023-04-23 17:26:28 -0700

    use web component dialog over sweetalerts

--- packages/widgets/src/components/nk-connect-wallet-button/readme.md
+++ packages/widgets/src/components/nk-connect-wallet-button/readme.md
@@ -7,27 +7,12 @@
 
 ## Shadow Parts
 
-| Part                     | Description                                       |
-| ------------------------ | ------------------------------------------------- |
-| `"wallet-btn"`           | Button for connecting to a cryptocurrency wallet. |
-| `"wallet-btn-container"` | Container that wraps the wallet button.           |
+| Part                     | Description |
+| ------------------------ | ----------- |
+| `"wallet-btn"`           |             |
+| `"wallet-btn-container"` |             |
 
 
-## Examples of overriding styles:
-  
-Add any overrides inside a `style` tag in your html `head`.
-
-    nk-connect-wallet-button::part(wallet-btn-container) {  
-        margin: 20px auto;  
-        display: block;  
-    }
-
-    nk-connect-wallet-button::part(wallet-btn) {  
-        width: 300px;  
-        height: 50px;  
-        background: red;  
-    }
-  
 ----------------------------------------------
 
 *Built with [StencilJS](https://stenciljs.com/)*
---
+++ packages/widgets/src/components/nk-dialog/nk-dialog.tsx
@@ -0,0 +1,69 @@
+import { MDCDialog } from '@material/dialog';
+import { Component, h, Prop } from '@stencil/core';
+
+@Component({
+  tag: 'nk-dialog',
+  styleUrl: 'nk-dialog.scss',
+  shadow: true,
+})
+export class NKDialog {
+  container!: HTMLDivElement;
+
+  dialog: MDCDialog;
+
+  @Prop() title = '';
+
+  @Prop() open = false;
+
+  @Prop() buttonText = 'Confirm';
+
+  componentDidLoad() {
+    this.dialog = new MDCDialog(this.container);
+
+    if (this.open) {
+      this.dialog.open();
+    }
+  }
+
+  componentDidUpdate() {
+    if (this.open) {
+      this.dialog.open();
+    }
+  }
+
+  render() {
+    return (
+      <div
+        class="mdc-dialog"
+        ref={(el) => (this.container = el as HTMLDivElement)}>
+        <div class="mdc-dialog__container">
+          <div
+            class="mdc-dialog__surface"
+            role="alertdialog"
+            aria-modal="true"
+            aria-labelledby="my-dialog-title"
+            aria-describedby="my-dialog-content">
+            {this.title.length > 0 && (
+              <h2 class="mdc-dialog__title" id="my-dialog-title">
+                {this.title}
+              </h2>
+            )}
+            <div class="mdc-dialog__content" id="my-dialog-content">
+              <slot />
+            </div>
+            <div class="mdc-dialog__actions">
+              <button
+                type="button"
+                class="mdc-button mdc-dialog__button"
+                data-mdc-dialog-action="discard">
+                <div class="mdc-button__ripple"></div>
+                <span class="mdc-button__label">{this.buttonText}</span>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="mdc-dialog__scrim"></div>
+      </div>
+    );
+  }
+}
